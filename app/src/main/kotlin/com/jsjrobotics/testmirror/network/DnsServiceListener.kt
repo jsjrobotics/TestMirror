de85254b820ecf12aec8b54c79aceffe11802817
jsjrobotics <jonathanj@mirror.co>
2018-12-25 00:29:28 -0500

    updated

--- app/src/main/kotlin/com/jsjrobotics/testmirror/network/DnsServiceListener.kt
+++ app/src/main/kotlin/com/jsjrobotics/testmirror/network/DnsServiceListener.kt
@@ -3,40 +3,59 @@ package com.jsjrobotics.testmirror.network
 import android.net.nsd.NsdManager
 import android.net.nsd.NsdServiceInfo
 import android.util.Log
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
+import javax.inject.Singleton
 
+@Singleton
+class DnsServiceListener(val nsdManager: NsdManager) {
 
-class DnsServiceListener(val nsdManager: NsdManager) : NsdManager.DiscoveryListener {
+    private val serviceInfoDiscovered : PublishSubject<NsdServiceInfo> = PublishSubject.create()
+    val onServiceInfoDiscovered : Observable<NsdServiceInfo> = serviceInfoDiscovered
     private val TAG = javaClass.simpleName
+    private val serviceType = "_mirror._tcp."
+    private val serviceNameMatcher = Regex("^mirror-|^Android$")
+    private var resolvedServiceInfo: NsdServiceInfo? = null
 
-    override fun onServiceFound(service: NsdServiceInfo) {
-        // A service was found! Do something with it.
-        Log.d(TAG, "Service discovery success$service")
-        when {
-            service.serviceType != SERVICE_TYPE -> // Service type is the string containing the protocol and
-                // transport layer for this service.
-                Log.d(TAG, "Unknown Service Type: ${service.serviceType}")
-            service.serviceName == mServiceName -> // The name of the service tells the user what they'd be
-                // connecting to. It could be "Bob's Chat App".
-                Log.d(TAG, "Same machine: $mServiceName")
-            service.serviceName.contains("NsdChat") -> mNsdManager.resolveService(service, mResolveListener)
+    private val discoveryListener : NsdManager.DiscoveryListener = object : NsdManager.DiscoveryListener {
+        override fun onServiceFound(service: NsdServiceInfo) {
+            if (service.serviceType == serviceType && serviceNameMatcher.matches(service.serviceName)) {
+                nsdManager.resolveService(service, buildResolveListener())
+            }
+        }
+
+
+        override fun onStartDiscoveryFailed(serviceType: String, errorCode: Int) {
+            Log.e(TAG, "Discovery failed: Error code:$errorCode")
+            nsdManager.stopServiceDiscovery(this)
+        }
+
+        override fun onStopDiscoveryFailed(serviceType: String, errorCode: Int) {
+            Log.e(TAG, "Discovery failed: Error code:$errorCode")
+            nsdManager.stopServiceDiscovery(this)
+        }
+
+
+        override fun onDiscoveryStarted(serviceType: String) { /* Do Nothing */ }
+
+        override fun onDiscoveryStopped(serviceType: String) { /* Do Nothing */ }
+
+        override fun onServiceLost(serviceInfo: NsdServiceInfo) { /* Do Nothing */ }
+    }
+
+    private fun buildResolveListener() : NsdManager.ResolveListener {
+        return object : NsdManager.ResolveListener {
+            override fun onResolveFailed(serviceInfo: NsdServiceInfo, errorCode: Int) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onServiceResolved(serviceInfo: NsdServiceInfo) {
+                resolvedServiceInfo = serviceInfo
+            }
         }
     }
 
-
-    override fun onStartDiscoveryFailed(serviceType: String, errorCode: Int) {
-        Log.e(TAG, "Discovery failed: Error code:$errorCode")
-        nsdManager.stopServiceDiscovery(this)
+    fun discoverServices() {
+        nsdManager.discoverServices(serviceType, NsdManager.PROTOCOL_DNS_SD, discoveryListener )
     }
-
-    override fun onStopDiscoveryFailed(serviceType: String, errorCode: Int) {
-        Log.e(TAG, "Discovery failed: Error code:$errorCode")
-        nsdManager.stopServiceDiscovery(this)
-    }
-
-
-    override fun onDiscoveryStarted(serviceType: String) { /* Do Nothing */}
-
-    override fun onDiscoveryStopped(serviceType: String) { /* Do Nothing */}
-
-    override fun onServiceLost(serviceInfo: NsdServiceInfo) { /* Do Nothing */}
 }
